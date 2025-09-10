671d85e84f0a3e044d88f21f583ee9dc30496d34
Smile-Khan <ismailkhan.pathan19@gmail.com>
2025-09-10 17:43:25 +0530

    SOLUTION ADDED Convert Sorted Array to Binary search Tree

--- LinkedList/108. Convert Sorted Array to Binary Search Tree.java
+++ LinkedList/108. Convert Sorted Array to Binary Search Tree.java
@@ -40,3 +40,29 @@ class Solution {
         return root;
     }
 }
+
+// SOLUTION: Approach 2: Alternative Middle Calculation
+
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+    
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
+        
+        // Two ways to choose middle:
+        // Option 1: Always choose left-middle for even length
+        int mid = start + (end - start) / 2;
+        
+        // Option 2: Always choose right-middle for even length  
+        // int mid = start + (end - start + 1) / 2;
+        
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums, start, mid - 1);
+        node.right = helper(nums, mid + 1, end);
+        
+        return node;
+    }
+}
