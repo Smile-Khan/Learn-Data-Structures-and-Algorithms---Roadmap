7cbd92970c746025de47d21e713c1be31e423039
Smile-Khan <ismailkhan.pathan19@gmail.com>
2025-10-30 19:34:41 +0530

    1526. Minimum Number of Increments on Subarrays to Form a Target Array.java

---
+++ GREEDY/1526. Minimum Number of Increments on Subarrays to Form a Target Array.java
@@ -0,0 +1,18 @@
+// QUESTION: 1526. Minimum Number of Increments on Subarrays to Form a Target Array
+// LINK: https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/description/?envType=daily-question&envId=2025-10-30
+
+// SOLUTION: GREEDY 
+
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int operations = target[0]; // Start with first element
+        
+        for (int i = 1; i < target.length; i++) {
+            if (target[i] > target[i - 1]) {
+                operations += target[i] - target[i - 1];
+            }
+        }
+        
+        return operations;
+    }
+}
